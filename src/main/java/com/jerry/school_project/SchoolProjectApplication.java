3f382cfee8421b51048967b478d0158f3ac97a91
Jerry <you@example.com>
2025-05-14 14:41:56 +0200

    added basic rest controller

--- src/main/java/com/jerry/school_project/SchoolProjectApplication.java
+++ src/main/java/com/jerry/school_project/SchoolProjectApplication.java
@@ -2,6 +2,8 @@ package com.jerry.school_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class SchoolProjectApplication {
@@ -11,6 +13,14 @@ public class SchoolProjectApplication {
         System.out.println("Welcome to School Project");
         System.out.println("Testing again");
 
+    }
+
+    @RestController
+    class HiController {
+        @GetMapping("/hi")
+        public String say() {
+            return "Hi from Spring Boot!";
+        }
     }
 
 }
