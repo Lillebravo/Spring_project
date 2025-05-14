c363c897a54853cd62a3370b9d38d3e3d7b740ee
Jerry <you@example.com>
2025-05-14 12:50:15 +0200

    First commit

---
+++ src/main/java/com/jerry/school_project/SchoolProjectApplication.java
@@ -0,0 +1,14 @@
+package com.jerry.school_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolProjectApplication.class, args);
+        System.out.println("Welcome to School Project");
+    }
+
+}
